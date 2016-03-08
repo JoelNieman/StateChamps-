53a8bf8ad957f80bc74deb51a4158a9acba28ed8
Joel Nieman <niemanjoel@gmail.com>
2016-03-07 22:42:49 -0500

    added thumbnail images and changed initial video load

---
+++ StateChamps!/StateChamps!/ArticlesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ArticlesViewController.swift
+//  StateChamps!
+//
+//  Created by Joel Nieman on 3/7/16.
+//  Copyright © 2016 JoelNieman. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    @IBOutlet weak var header: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        formatViewController()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- StateChamps!/StateChamps!/HomeViewController.swift
+++ StateChamps!/StateChamps!/HomeViewController.swift
@@ -11,42 +11,83 @@ import YouTubePlayer
 //import Parse
 
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController , APIOnResponseDelegate, UITableViewDelegate, UITableViewDataSource{
+    
 
+    @IBOutlet var showVideosTableView: UITableView!
     @IBOutlet weak var header: UIView!
     @IBOutlet var playerView: YouTubePlayerView!
     
-    let myVideoURL = NSURL(string: "https://www.youtube.com/watch?v=wQg3bXrVLtg")
-    var playerState = YouTubePlayerState(rawValue: "")
+    var myVideoID: String?
+    var showVideos = [SCVideo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         formatViewController()
-        loadVideo()
-        fetchShowVideos()
-
+//        loadVideo()
+        let apiCall = YouTubeAPICall(handler: self)
+        apiCall.fetchShowVideos()
+        
+        
+        
+        showVideosTableView.rowHeight = 80
+        
     }
     
     override func viewWillDisappear(animated: Bool) {
         playerView.stop()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     
     //  This sets up the video. The values (0 or 1) change the properties of the video player
     
-    func loadVideo() {
+//    func loadVideo() {
+//        playerView.playerVars = [
+//            "playsinline": "1",
+//            "controls": "1",
+//            "showinfo": "0"
+//        ]
+//        playerView.loadVideoID(myVideoID!)
+//    }
+    
+    //  Upon finishing the YouTubeAPI call, showVideos is populated witht the SCVideos
+    //  and the tableview is reloaded.
+    
+    func onResponse(SCVideos: [SCVideo]) {
+        showVideos = SCVideos
+        showVideosTableView.reloadData()
+        
+        myVideoID = showVideos[0].videoID
         playerView.playerVars = [
             "playsinline": "1",
             "controls": "1",
             "showinfo": "0"
         ]
-        playerView.loadVideoURL(myVideoURL!)
+        playerView.loadVideoID(myVideoID!)
     }
     
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return showVideos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("SCVideoCell") as! VideoCell!
+        
+        let videoDetails = showVideos[indexPath.row]
+        cell.titleOutlet.text = videoDetails.title as String
+        cell.thumbnailOutlet.image = videoDetails.thumbnailImage
+        
+        return cell
+    }
+    
 }
 
